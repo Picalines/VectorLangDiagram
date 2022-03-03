b0367515c5ba2e20bfb6bc7676fdd75f1edce8cb
Picalines <picalines0@gmail.com>
2022-03-04 00:13:54 +0300

    monadic Parser.SelectMany & Parser.Parse

---
+++ VectorLang/Parsing/ParserExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using VectorLang.Tokenization;
+
+namespace VectorLang.Parsing;
+
+internal static class ParserExtensions
+{
+    public static Parser<V> SelectMany<T, U, V>(this Parser<T> first, Func<T, Parser<U>> second, Func<T, U, V> select)
+    {
+        return first.Then(firstResult => second(firstResult).Select(secondResult => select(firstResult, secondResult)));
+    }
+
+    public static IParseResult<T> Parse<T>(this Parser<T> parser, string input)
+    {
+        var parseInput = new ParseInput(Tokenizer.Tokenize(input));
+
+        try
+        {
+            return parser(parseInput);
+        }
+        catch (Exception exception)
+        {
+            return ParseResult<T>.Failure($"Exception: {exception.Message}", parseInput);
+        }
+    }
+}
