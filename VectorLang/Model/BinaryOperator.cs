91d91db53acd806a168d6fab8cb2c76dfc9eefbb
Picalines <picalines0@gmail.com>
2022-03-04 13:25:53 +0300

    Operator enums

---
+++ VectorLang/Model/BinaryOperator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace VectorLang.Model;
+
+public enum BinaryOperator
+{
+    [Description("+")] Add,
+    [Description("-")] Subtract,
+    [Description("*")] Multiply,
+    [Description("/")] Divide,
+    [Description("%")] Modulo,
+}
---
+++ VectorLang/Model/UnaryOperator.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace VectorLang.Model;
+
+public enum UnaryOperator
+{
+    [Description("+")] Plus,
+    [Description("-")] Minus,
+}
