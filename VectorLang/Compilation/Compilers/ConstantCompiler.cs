b0f66773519e904906fdeebff290211681367b98
Picalines <picalines0@gmail.com>
2022-04-01 02:18:49 +0300

    T.Yield & ConstantCompiler

---
+++ VectorLang/Compilation/Compilers/ConstantCompiler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using VectorLang.Model;
+using VectorLang.SyntaxTree;
+
+namespace VectorLang.Compilation;
+
+internal static class ConstantCompiler
+{
+    public static IEnumerable<Instruction> Compile(ConstantNode constant) => constant switch
+    {
+        NumberNode { Value: var number } => new PushNumberInstruction(new NumberInstance(number)).Yield(),
+
+        StringNode { Value: var str } => new PushStringInstruction(new StringInstance(str)).Yield(),
+
+        VectorNode { X: var x, Y: var y } => new PushVectorInstruction(new VectorInstance(x, y)).Yield(),
+
+        ColorNode { R: var r, G: var g, B: var b } => new PushColorInstruction(new ColorInstance(r, g, b)).Yield(),
+
+        _ => throw new NotImplementedException(),
+    };
+}
---
+++ VectorLang/UtilityExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VectorLang;
+
+internal static class UtilityExtensions
+{
+    public static IEnumerable<T> Yield<T>(this T item)
+    {
+        yield return item;
+    }
+}
