856e0d21d87309cc4fc5f5d3c792fe64a550ebbe
Picalines <picalines0@gmail.com>
2022-04-01 01:38:32 +0300

    ColorInstance

---
+++ VectorLang/Model/Instance/Types/ColorInstance.cs
@@ -0,0 +1,28 @@
+﻿namespace VectorLang.Model;
+
+internal sealed class ColorInstance : ReflectionInstance
+{
+    [ReflectionInstanceType]
+    public static readonly ReflectionInstanceType InstanceType = ReflectionInstanceType.Of<ColorInstance>("color");
+
+    [InstanceField("r")]
+    public NumberInstance R { get; }
+
+    [InstanceField("g")]
+    public NumberInstance G { get; }
+
+    [InstanceField("b")]
+    public NumberInstance B { get; }
+
+    public ColorInstance(NumberInstance r, NumberInstance g, NumberInstance b) : base(InstanceType)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    public ColorInstance(double r, double g, double b)
+        : this(new NumberInstance(r), new(g), new(b))
+    {
+    }
+}
