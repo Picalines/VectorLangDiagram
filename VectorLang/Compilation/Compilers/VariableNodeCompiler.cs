ab049f393a0c15bcdc22a331fff3cc3f636cd7a3
Picalines <picalines0@gmail.com>
2022-04-01 12:35:20 +0300

    variable node compiler

---
+++ VectorLang/Compilation/Compilers/VariableNodeCompiler.cs
@@ -0,0 +1,24 @@
+﻿using VectorLang.Model;
+using VectorLang.SyntaxTree;
+
+namespace VectorLang.Compilation;
+
+internal static class VariableNodeCompiler
+{
+    public static CompiledExpression Compile(SymbolTable symbols, VariableNode variableNode)
+    {
+        var variableName = variableNode.Token.Value;
+
+        symbols.TryLookup(variableName, out VariableSymbol? variableSymbol);
+
+        if (variableSymbol is null)
+        {
+            throw ProgramException.At(variableNode.Selection, new UndefinedException($"variable '{variableName}'"));
+        }
+
+        return new(
+            type: variableSymbol.Type,
+            instruction: new LoadInstruction(variableSymbol.Address)
+        );
+    }
+}
