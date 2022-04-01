35a07b0ce69f9482544aa3f2cc9b02056df23333
Picalines <picalines0@gmail.com>
2022-04-01 12:12:34 +0300

    ProgramException useful methods + UndefinedException

--- VectorLang/Model/Exceptions/ProgramException.cs
+++ VectorLang/Model/Exceptions/ProgramException.cs
@@ -9,4 +9,41 @@ public sealed class ProgramException : Exception
 
     internal ProgramException(string message, Exception? innerException = null)
         : base(message, innerException) { }
+
+    internal ProgramException(Exception innerException)
+        : base(innerException.Message, innerException) { }
+
+    internal static ProgramException At(TextSelection selection, Exception exception) => new(exception) { Location = selection };
+
+    internal static T MaybeAt<T>(TextSelection selection, Func<T> func)
+    {
+        try
+        {
+            return func();
+        }
+        catch (ProgramException)
+        {
+            throw;
+        }
+        catch (Exception exception)
+        {
+            throw new ProgramException(exception) { Location = selection };
+        }
+    }
+
+    internal static void MaybeAt(TextSelection selection, Action action)
+    {
+        try
+        {
+            action();
+        }
+        catch (ProgramException)
+        {
+            throw;
+        }
+        catch (Exception exception)
+        {
+            throw new ProgramException(exception) { Location = selection };
+        }
+    }
 }
---
+++ VectorLang/Model/Exceptions/UndefinedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VectorLang.Model;
+
+public sealed class UndefinedException : Exception
+{
+    public UndefinedException(string value) : base($"{value} is undefined") { }
+
+    internal static UndefinedException TypeMember(InstanceType type, string fieldOrMethod) => new($"field or method {type.FormatMember(fieldOrMethod)}");
+
+    internal static UndefinedException TypeMember(InstanceType type, string method, params InstanceType[] argumentTypes) => new($"method {type.FormatMember(method, argumentTypes)}");
+
+    internal static UndefinedException TypeMember(InstanceType type, UnaryOperator unaryOperator) => new($"unary operator {type.FormatMember(unaryOperator)}");
+
+    internal static UndefinedException TypeMember(InstanceType type, BinaryOperator binaryOperator, InstanceType rightType) => new($"binary operator {type.FormatMember(binaryOperator, rightType)}");
+}
