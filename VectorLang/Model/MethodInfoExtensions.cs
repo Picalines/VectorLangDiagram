1e055d37bc7ab7c984ceb9a7c9bd90dad9f1cd7a
Picalines <picalines0@gmail.com>
2022-04-05 02:09:39 +0300

    InvokeAndRethrow

---
+++ VectorLang/Model/MethodInfoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace VectorLang.Model;
+
+internal static class MethodInfoExtensions
+{
+    public static object? InvokeAndRethrow(this MethodInfo methodInfo, object? obj, object?[]? parameters)
+    {
+        try
+        {
+            return methodInfo.Invoke(obj, parameters);
+        }
+        catch (TargetInvocationException targetInvocationException)
+        {
+            throw targetInvocationException.InnerException!;
+        }
+    }
+}
