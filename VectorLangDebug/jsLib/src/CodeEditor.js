258ea283164f0ded6cf18baee6c0dd89f86d9684
Picalines <picalines0@gmail.com>
2022-04-05 16:39:46 +0300

    code editor component

---
+++ VectorLangDebug/jsLib/src/CodeEditor.js
@@ -0,0 +1,46 @@
+﻿import * as CodeMirror from 'codemirror';
+
+import './vectorLangMode';
+
+const errorStyle = `
+    text-decoration: red wavy underline;
+    background-color: #ba060644;
+    box-shadow: #ba060622 0px 0px 10px;
+`;
+
+export class CodeEditor {
+    #editor
+
+    constructor(textareaSelector, initialProgram = '') {
+        const textarea = document.querySelector(textareaSelector);
+
+        this.#editor = CodeMirror.fromTextArea(textarea, {
+            value: initialProgram,
+            lineNumbers: true,
+            theme: "material",
+            mode: "vectorLang",
+        });
+
+        this.#editor.on('change', () => {
+            textarea.innerHTML = this.#editor.getValue();
+            textarea.dispatchEvent(new Event('change', { bubbles: true }));
+        });
+    }
+
+    focus() {
+        this.#editor.focus();
+    }
+
+    markError(selection) {
+        const { start, end } = selection;
+
+        const from = { line: start.line - 1, ch: start.column - 1 };
+        const to = { line: end.line - 1, ch: end.column - 1 };
+
+        this.#editor.getDoc().markText(from, to, { css: errorStyle });
+    }
+
+    clearErrors() {
+        this.#editor.doc.getAllMarks().forEach(marker => marker.clear());
+    }
+}
--- VectorLangDebug/jsLib/src/index.js
+++ VectorLangDebug/jsLib/src/index.js
@@ -1,45 +1,12 @@
-﻿import * as CodeMirror from 'codemirror';
+﻿import { CodeEditor } from './CodeEditor';
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 
-import './vectorLangMode'
-
-const errorStyle = `
-    text-decoration: red wavy underline;
-    background-color: #ba060644;
-    box-shadow: #ba060622 0px 0px 10px;
-`;
-
-export function initializeCodeMirror(textAreaSelector, initialProgram = "") {
-    const textarea = document.querySelector(textAreaSelector);
-
-    const editor = CodeMirror.fromTextArea(textarea, {
-        value: initialProgram,
-        lineNumbers: true,
-        theme: "material",
-        mode: "vectorLang",
-    });
-
-    editor.on('change', () => {
-        textarea.innerHTML = editor.getValue();
-        textarea.dispatchEvent(new Event('change', { bubbles: true }));
-    });
+export function initializeCodeEditor(textAreaSelector, initialProgram = "") {
+    const editor = new CodeEditor(textAreaSelector, initialProgram);
 
     editor.focus();
 
     return editor;
-}
-
-export function markError(editor, selection) {
-    const { start, end } = selection;
-
-    const from = { line: start.line - 1, ch: start.column - 1 };
-    const to = { line: end.line - 1, ch: end.column - 1 };
-
-    editor.getDoc().markText(from, to, { css: errorStyle });
-}
-
-export function clearErrors(editor) {
-    editor.doc.getAllMarks().forEach(marker => marker.clear());
 }
