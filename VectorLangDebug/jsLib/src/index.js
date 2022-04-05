fa2eb6b3268457990b92433db129027e7026c173
Picalines <picalines0@gmail.com>
2022-04-05 13:49:23 +0300

    simple syntax highlighting

--- VectorLangDebug/jsLib/src/index.js
+++ VectorLangDebug/jsLib/src/index.js
@@ -1,3 +1,24 @@
-﻿export function hiFromJS() {
-    alert("hi from js!");
+﻿import * as CodeMirror from 'codemirror';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import './vectorLangMode'
+
+export function initializeCodeMirror(textAreaSelector, initialProgram = "") {
+    const textarea = document.querySelector(textAreaSelector);
+
+    const editor = CodeMirror.fromTextArea(textarea, {
+        value: initialProgram,
+        lineNumbers: true,
+        theme: "material",
+        mode: "vectorLang",
+    });
+    
+    editor.on('change', () => {
+        textarea.innerHTML = editor.getValue();
+        textarea.dispatchEvent(new Event('change', { bubbles: true }));
+    });
+
+    editor.focus();
 }
---
+++ VectorLangDebug/jsLib/src/vectorLangMode.js
@@ -0,0 +1,44 @@
+﻿import * as CodeMirror from 'codemirror';
+
+import 'codemirror/addon/mode/simple';
+
+CodeMirror.defineSimpleMode("vectorLang", {
+    start: [
+        {
+            regex: /(\d+(\.\d+)?|\.\d+)\w*/,
+            token: "number"
+        },
+        {
+            regex: /""|''|(['"]).*?[^\\]\1/,
+            token: "string"
+        },
+        {
+            regex: /#[a-fA-F\d]{6}/,
+            token: "color"
+        },
+        {
+            regex: /\b(def|external|val|plot)\b/,
+            token: "keyword"
+        },
+        {
+            regex: /\/\/.*/,
+            token: "comment"
+        },
+        {
+            regex: /[-+\/*=>!\.]+/,
+            token: "operator"
+        },
+        {
+            regex: /[\{\[\(]/,
+            indent: true
+        },
+        {
+            regex: /[\}\]\)]/,
+            dedent: true
+        },
+        {
+            regex: /\b[a-zA-Z_][a-zA-Z0-9_]*\b/,
+            token: "variable-3",
+        },
+    ],
+});
