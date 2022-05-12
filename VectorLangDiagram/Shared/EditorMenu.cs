69e2b3a0b87a088b7f5ef5a943f506fcfef36e88
Picalines <picalines0@gmail.com>
2022-05-12 23:38:41 +0300

    view submenu

--- VectorLangDiagram/Shared/EditorMenu.cs
+++ VectorLangDiagram/Shared/EditorMenu.cs
@@ -6,6 +6,9 @@ namespace VectorLangDiagram.Shared;
 
 public sealed class EditorMenu : ComponentBase
 {
+    [Parameter]
+    public EventCallback<EditorWindows> OnViewSelected { get; set; }
+
     protected override void OnInitialized()
     {
         base.OnInitialized();
@@ -17,48 +20,66 @@ public sealed class EditorMenu : ComponentBase
 
         Electron.Menu.SetApplicationMenu(new[]
         {
-            new MenuItem()
-            {
-                Label = "File",
-                Submenu = new MenuItem[]
-                {
-                    new() { Label = "Open", Accelerator = "CommandOrControl+O" },
-                    new() { Label = "Save", Accelerator = "CommandOrControl+S" },
-                    new() { Label = "Save as...", Accelerator = "CommandOrControl+Shift+S" },
-                    new() { Type = MenuType.separator },
-                    new() { Role = MenuRole.quit, Accelerator = "Alt+F4" },
-                },
-            },
-
-            new MenuItem()
-            {
-                Role = MenuRole.editMenu,
-                Submenu = new MenuItem[]
-                {
-                    new() { Role = MenuRole.undo },
-                    new() { Role = MenuRole.redo },
-                }
-            },
-
-            new MenuItem()
-            {
-                Role = MenuRole.windowMenu,
-                Submenu = new[]
-                {
-                    new MenuItem() { Role = MenuRole.togglefullscreen },
-
-                    new MenuItem()
-                    {
-                        Label = "View",
-                        Submenu = new MenuItem[]
-                        {
-                            new() { Type = MenuType.radio, Label = "Code and Diagram" },
-                            new() { Type = MenuType.radio, Label = "Code only" },
-                            new() { Type = MenuType.radio, Label = "Diagram only" },
-                        }
-                    },
-                },
-            },
+            CreateFileMenu(),
+            CreateEditMenu(),
+            CreateWindowMenu(),
         });
+    }
+
+    private static MenuItem CreateFileMenu() => new()
+    {
+        Label = "File",
+        Submenu = new MenuItem[]
+        {
+            new() { Label = "Open", Accelerator = "CommandOrControl+O" },
+            new() { Label = "Save", Accelerator = "CommandOrControl+S" },
+            new() { Label = "Save as...", Accelerator = "CommandOrControl+Shift+S" },
+            new() { Type = MenuType.separator },
+            new() { Role = MenuRole.quit, Accelerator = "Alt+F4" },
+        },
+    };
+
+    private static MenuItem CreateEditMenu() => new()
+    {
+        Role = MenuRole.editMenu,
+        Submenu = new MenuItem[]
+        {
+            new() { Role = MenuRole.undo },
+            new() { Role = MenuRole.redo },
+        }
+    };
+
+    private MenuItem CreateWindowMenu() => new()
+    {
+        Role = MenuRole.windowMenu,
+        Submenu = new[]
+        {
+            new MenuItem() { Role = MenuRole.togglefullscreen },
+
+            CreateViewMenu(),
+
+#if DEBUG
+            new MenuItem() { Role = MenuRole.toggledevtools },
+#endif
+        },
+    };
+
+    private MenuItem CreateViewMenu()
+    {
+        Action SelectViewMode(EditorWindows windows)
+        {
+            return () => InvokeAsync(() => OnViewSelected.InvokeAsync(windows));
+        }
+
+        return new()
+        {
+            Label = "View",
+            Submenu = new MenuItem[]
+            {
+                new() { Click = SelectViewMode(EditorWindows.Code | EditorWindows.Diagram), Label = "Code and Diagram" },
+                new() { Click = SelectViewMode(EditorWindows.Code), Label = "Code only" },
+                new() { Click = SelectViewMode(EditorWindows.Diagram), Label = "Diagram only" },
+            }
+        };
     }
 }
---
+++ VectorLangDiagram/Shared/EditorWindows.cs
@@ -0,0 +1,8 @@
+﻿namespace VectorLangDiagram.Shared;
+
+[Flags]
+public enum EditorWindows
+{
+    Code = 1,
+    Diagram = 2,
+}
