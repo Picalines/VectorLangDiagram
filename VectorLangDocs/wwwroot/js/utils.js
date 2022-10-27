104cb0ad558c5f9f19d785c8084b9893bd8afd87
Picalines <picalines0@gmail.com>
2022-10-28 00:43:06 +0300

    DocRepoViewer nav links

---
+++ VectorLangDocs/wwwroot/js/utils.js
@@ -0,0 +1,26 @@
+﻿function BlazorScrollIntoView(selector) {
+    const element = document.querySelector(selector);
+    if (!(element instanceof HTMLElement)) {
+        return;
+    }
+
+    element.scrollIntoView();
+}
+
+function BlazorAddTemporaryClass(selector, className, durationMs) {
+    const element = document.querySelector(selector);
+    if (!(element instanceof HTMLElement)) {
+        return;
+    }
+
+    element.__temporaryClassTimeouts ??= {};
+
+    element.classList.add(className);
+    const timeout = setTimeout(() => {
+        element.classList.remove(className);
+        delete element.__temporaryClassTimeouts[className];
+    }, durationMs);
+
+    clearTimeout(element.__temporaryClassTimeouts[className])
+    element.__temporaryClassTimeouts[className] = timeout;
+}
